f7ff6891618f37ae3a0ada03ece2ab13eac595d5
Shamir Khodzha <khodzha.sh@gmail.com>
2012-07-28 21:44:22 +0300

    task 201 almost done

--- 201/main.cpp
+++ 201/main.cpp
@@ -1,27 +1,27 @@
-// WORK IN PROGRESS
+// WORK IN PROGRESS : works right, but timelimit exceeded, need to rewrite in another way
 #include <iostream>
 
 int total=0;
-int func(int k);
+int func(int k, int max);
 
 int main(void)
 {
   int N;
   std::cin>>N;
-  func(N);
+  func(N, 4);
   std::cout<<total;
 }
 
-int func(int k)
+int func(int k, int max)
 {
-  if(k==0) { total++; return 0; }
+  if(k==0||k==1) { total++; return 0; }
   int j;
-  for(j=((k<4)?k:4);j>0;j--)
+  for(j=max;j>0;j--)
   {
     if((k-j)>=0)
     {
-      std::cout<<"debug k and j "<<k<<" "<<j<<"\n";
-      func(k%j);
+      //std::cout<<"debug k and j "<<k<<" "<<j<<"\n";
+      func(k-j,j);
     }
   }
   return 0;
